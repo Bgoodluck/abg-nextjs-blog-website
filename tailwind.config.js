74bbab2c87fd2f1dc1cff3568a9f5ff89b2488f1
unknown <Bgoodluck>
2024-10-02 05:59:23 +0100

    dynamic purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -14,4 +33,16 @@ module.exports = {
     },
   },
   plugins: [],
+  safelist: [
+    'bg-black', // Add any specific classes you are using dynamically
+    'text-white',
+    'py-1',
+    'px-4',
+    'rounded-sm',
+    'gap-1', // Add any other classes you might need
+    'gap-y-10',
+    'mb-16',
+    'xl:mx-24',
+    // You can also add other dynamic classes if necessary
+  ],
 };
