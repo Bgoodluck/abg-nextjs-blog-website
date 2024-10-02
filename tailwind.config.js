c3e006e5be2f6c4f1a57184c1d7f61597645f825
unknown <Bgoodluck>
2024-10-02 16:32:36 +0100

    purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,11 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
