b535d88087f3f2ae67edd2ef3f61af69338513d4
unknown <Bgoodluck>
2024-10-02 16:39:32 +0100

    src

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@
 module.exports = {
   important: true,
   content: [
+    "./src/**/*.{js, ts, jsx, tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
