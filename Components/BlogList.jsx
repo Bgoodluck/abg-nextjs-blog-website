be2c8009c8b641e4de8a1225b517ece3dec4cf6e
unknown <Bgoodluck>
2024-10-02 06:07:47 +0100

    space

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -25,7 +25,7 @@ function BlogList() {
 
   return (
     <div>
-       <div className=' flex justify-center gap-6 my-10'>
+       <div className='flex justify-center gap-6 my-10'>
            <button onClick={()=> setMenu('All')} className={menu === "All" ? ` bg-black text-white py-1 px-4 rounded-sm` : ""}>All</button>
            <button onClick={()=> setMenu('Technology')} className={menu === "Technology" ? ` bg-black text-white py-1 px-4 rounded-sm` : ""}>Technology</button>
            <button onClick={()=> setMenu('Startup')} className={menu === "Startup" ? ` bg-black text-white py-1 px-4 rounded-sm` : ""}>Startup</button>
