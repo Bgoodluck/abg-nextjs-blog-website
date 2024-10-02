e8cd2a2eb7db2658b47b950e21c172a299ae93e1
unknown <Bgoodluck>
2024-10-02 06:04:44 +0100

    classes

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -26,10 +26,27 @@ function BlogList() {
   return (
     <div>
        <div className=' flex justify-center gap-6 my-10'>
-           <button onClick={()=> setMenu('All')} className={menu === "All" ? ` bg-black text-white py-1 px-4 rounded-sm` : ""}>All</button>
-           <button onClick={()=> setMenu('Technology')} className={menu === "Technology" ? ` bg-black text-white py-1 px-4 rounded-sm` : ""}>Technology</button>
-           <button onClick={()=> setMenu('Startup')} className={menu === "Startup" ? ` bg-black text-white py-1 px-4 rounded-sm` : ""}>Startup</button>
-           <button onClick={()=> setMenu('Lifestyle')} className={menu === "Lifestyle" ? ` bg-black text-white py-1 px-4 rounded-sm` : ""}>Lifestyle</button>
+       <button 
+  onClick={() => setMenu('All')} 
+  className={`bg-black text-white py-1 px-4 rounded-sm ${menu === "All" ? "bg-opacity-75" : ""}`}>
+  All
+</button>
+<button 
+  onClick={() => setMenu('Technology')} 
+  className={`bg-black text-white py-1 px-4 rounded-sm ${menu === "Technology" ? "bg-opacity-75" : ""}`}>
+  Technology
+</button>
+<button 
+  onClick={() => setMenu('Startup')} 
+  className={`bg-black text-white py-1 px-4 rounded-sm ${menu === "Startup" ? "bg-opacity-75" : ""}`}>
+  Startup
+</button>
+<button 
+  onClick={() => setMenu('Lifestyle')} 
+  className={`bg-black text-white py-1 px-4 rounded-sm ${menu === "Lifestyle" ? "bg-opacity-75" : ""}`}>
+  Lifestyle
+</button>
+
        </div>
        <div className=' flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
           {
